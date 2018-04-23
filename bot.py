acd14e4a591b2a5f5ebe8a732b79082d93fcf88b
destimon <destaom@gmail.com>
2018-04-23 23:53:29 +0300

    Case sensetive and substring Hello

--- bot.py
+++ bot.py
@@ -5,8 +5,11 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def echo_message(msg):
-	if msg.text == "Hello":
+	if msg.text.lower() == "hello":
 		bot.send_message(msg.chat.id, "Hi!")
+	elif msg.text.lower().find("hello") > 0:
+		bot.send_message(msg.chat.id, "You mean 'hello'?\nHello!")
+		
 	else:
 		bot.send_message(msg.chat.id, "I don't understand you, sorry.")
 
