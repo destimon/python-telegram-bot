602dc1e6a2b0a1156837d9b650f65a6483d25491
destimon <destaom@gmail.com>
2018-04-23 23:21:05 +0300

    simple response on Hello

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
-import config
+from config import config
 import telebot
 
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def echo_message(msg):
-	bot.send_message(msg.chat.id, msg.text)
+	if msg.text == "Hello":
+		bot.send_message(msg.chat.id, "Hi!")
+	else:
+		bot.send_message(msg.chat.id, "I don't understand you, sorry.")
 
 if __name__ == '__main__':
 	print("Start polling...")
