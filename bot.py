60e4e2084cc0b29e6ecf45cc8a30cf0ffba5fc71
Dmitry <destaom@gmail.com>
2018-04-24 19:21:26 +0300

    Bad words

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
-from config import config
 import telebot
-
+from profanity import profanity
+from config import config
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def echo_message(msg):
-	if msg.text.lower() == "hello":
+	if profanity.contains_profanity(msg.text):
+		bot.send_message(msg.chat.id, "Ew. Don't use words like this!") 
+	elif msg.text.lower() == "hello":
 		bot.send_message(msg.chat.id, "Hi!")
 	elif msg.text.lower().find("hello") > 0:
 		bot.send_message(msg.chat.id, "You mean 'hello'?\nHello!")
-		
 	else:
 		bot.send_message(msg.chat.id, "I don't understand you, sorry.")
 
